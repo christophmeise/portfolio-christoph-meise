c2699e9ccba986e5f5b4e388a5f754aa824bd64f
Shark919 <christoph.meise@gmx.de>
2021-04-26 10:00:23 +0200

    Move more font declarations to font.css, seems to be doing the job.

--- src/components/globalStyle.tsx
+++ src/components/globalStyle.tsx
@@ -7,29 +7,4 @@ body {
     margin: 0;
 }
 
-h1 {
-    font-size: 72px;
-    font-weight: 800;
-}
-h2 {
-    font-size: 48px;
-    font-weight: 800;
-}
-h3 {
-    font-size: 40px;
-    font-weight: 800;
-}
-h4 {
-    font-size: 28px;
-    font-weight: 800;
-}
-h5 {
-    font-size: 24px;
-    font-weight: 600;
-}
-h6 {
-    font-size: 20px;
-    font-weight: 500;
-}
-
 `;
