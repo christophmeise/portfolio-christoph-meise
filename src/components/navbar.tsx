e17718baeeccabf4c905c8a4f6ffecfa318c5494
Shark919 <christoph.meise@gmx.de>
2021-05-02 17:26:41 +0200

    Improve mobile navbar. Added Burgermenu with svg animation.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,12 +3,16 @@ import styled from 'styled-components';
 import { Button } from '../styles/button';
 import { Container } from '../styles/container';
 import { Logo } from '../styles/logo';
+import { device } from '../theme/theme';
 
 interface NavbarContainerProps {
   readonly inverted?: boolean;
 };
 interface HeaderProps {
   readonly fixed?: boolean;
+};
+interface BurgerMenuProps {
+  readonly sidebarOpened?: boolean;
 };
 
 const Header = styled.header<HeaderProps>`
@@ -31,8 +35,12 @@ const NavbarContainer = styled.div<NavbarContainerProps>`
     flex-direction: row;
     align-items: center;
     height: 40px;
-    grid-column: standard;
+    grid-column: full;
     padding: .5em 1em;
+
+    @media ${device.tablet} {
+        grid-column: standard;
+    }
 `;
 
 const MenuItem = styled.div<NavbarContainerProps>`
@@ -60,10 +68,66 @@ const NavbarButton = styled.div`
       display: none;
     }
 `;
-const BurgerMenu = styled.div`
+const BurgerMenu = styled.div<BurgerMenuProps>`
     margin-left: auto;
     visibility: hidden;
     display: none;
+    background-color: transparent;
+    border          : none;
+    cursor          : pointer;
+    display         : flex;
+    padding         : 0;
+    height          : 34px;
+    width           : 34px;
+    outline         : none;
+
+    svg {
+      height: 100%;
+      width : 100%;
+    }
+
+  .line {
+    fill        : none;
+    stroke      : ${props => props.theme.colors.fontWhite};
+    stroke-width: 6;
+    transition  : stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1),
+      stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  .line1 {
+    stroke-dasharray: 60 207;
+    stroke-width    : 6;
+  }
+
+  .line2 {
+    stroke-dasharray: 60 60;
+    stroke-width    : 6;
+  }
+
+  .line3 {
+    stroke-dasharray: 60 207;
+    stroke-width    : 6;
+  }
+
+  ${({ sidebarOpened }) => sidebarOpened && `
+    .line1 {
+    stroke-dasharray : 90 207;
+    stroke-dashoffset: -134;
+    stroke-width     : 6;
+  }
+
+  .line2 {
+    stroke-dasharray : 1 60;
+    stroke-dashoffset: -30;
+    stroke-width     : 6;
+  }
+
+   .line3 {
+    stroke-dasharray : 90 207;
+    stroke-dashoffset: -134;
+    stroke-width     : 6;
+  }
+  `}
 
     @media(max-width: 768px) {
       visibility: visible;
@@ -71,7 +135,20 @@ const BurgerMenu = styled.div`
     }
 `;
 
-export default class Navbar extends Component {
+interface NavbarState {
+  sidebarOpened: boolean;
+}
+export default class Navbar extends Component<any, NavbarState> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      sidebarOpened: false
+    };
+  }
+
+  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened });
+
   render() {
     return (
       <Header id="header">
@@ -86,7 +163,13 @@ export default class Navbar extends Component {
             <NavbarButton>
               <Button inverted>Let's talk</Button>
             </NavbarButton>
-            <BurgerMenu>_</BurgerMenu>
+            <BurgerMenu sidebarOpened={this.state.sidebarOpened} aria-label="Main Menu" onClick={() => this.handleToggle()}>
+              <svg fillOpacity="1" fillRule="evenodd" clipRule="evenodd" viewBox="0 0 100 100">
+                <path className="line line1" d="M 20,29.000046 H 80.00031 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
+                <path className="line line2" d="M 20,50 H 80" />
+                <path className="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" />
+              </svg>
+            </BurgerMenu>
           </NavbarContainer>
         </Container>
       </Header>
