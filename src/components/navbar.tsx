4c6463c1264139372a81a3fa1679b493d84f3efb
Shark919 <christoph.meise@gmx.de>
2021-05-02 17:14:43 +0200

    Navbar optimizations

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,7 @@ interface HeaderProps {
 };
 
 const Header = styled.header<HeaderProps>`
-    position: fixed;
+    position: absolute;
     width: 100%;
     top: 0;
     z-index: 100;
@@ -32,6 +32,7 @@ const NavbarContainer = styled.div<NavbarContainerProps>`
     align-items: center;
     height: 40px;
     grid-column: standard;
+    padding: .5em 1em;
 `;
 
 const MenuItem = styled.div<NavbarContainerProps>`
