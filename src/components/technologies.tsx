f6d33c7e13646756fa1ac28908acbf3f82692caf
Shark919 <christoph.meise@gmx.de>
2021-05-23 22:05:08 +0200

    Use planet for CV

--- src/components/technologies.tsx
+++ src/components/technologies.tsx
@@ -34,13 +34,29 @@ const GlobeContainer = styled.div`
 const GlobeTextContainer = styled.div`
 
     pointer-events: none;
-    h3, h4 {
+    position: relative;
+    h3, h4, p {
       color: ${(props) => props.theme.colors.fontWhite};
+    }
+
+    .cv-item {
+      margin-left: 30px;
     }
 
     @media ${device.laptop} {
       padding: 300px 0;
     }
+`;
+
+const TimelineContainer = styled.div`
+  background: ${(props) => props.theme.colors.primary} linear-gradient(180deg,${(props) => props.theme.colors.primary} 1rem,${(props) => props.theme.colors.primaryBg} 50vh);
+  content: '';
+  height:100%;
+  left: 0;
+  opacity:0.3;
+  position:absolute;
+  width:3px;
+
 `;
 
 const CustomGlobe: any = loadable(() => import(/* webpackPrefetch: true */ './customGlobe'));
@@ -69,7 +85,7 @@ export default class Technologies extends PureComponent {
                 </Scene>
               </ContainerContentStandard>
             </Container>
-            <Scene duration={1000} pin triggerHook={0.1}>
+            <Scene duration={2000} pin triggerHook={0.1}>
               {(progress: any) => (
                 <GlobeSection className="globe-trigger">
                   <Container style={{ height: '100vh' }}>
@@ -83,21 +99,50 @@ export default class Technologies extends PureComponent {
                         </GlobeWrapper>
                       </GlobeContainer>
                       <GlobeTextContainer>
-                        <Timeline totalProgress={progress} paused>
+                        <TimelineContainer />
+                        <Timeline
+                          totalProgress={progress}
+                          paused
+                          target={(
+                            <>
+                              <div className="cv-item">
+                                <h3>Head of UX/UI</h3>
+                                <p>WeWater gUG</p>
+                                <p>01/2021 - now</p>
+                              </div>
+                              <div className="cv-item">
+                                <h3>Senior Full-Stack Web Developer</h3>
+                                <p>DB Systel GmbH</p>
+                                <p>06/2020 - 07/2021</p>
+                              </div>
+                              <div className="cv-item">
+                                <h3>Senior Java Engineer</h3>
+                                <p>Serrala O2C Solutions GmbH</p>
+                                <p>07/2019 - 04/2020</p>
+                              </div>
+                            </>
+                          )}
+                        >
+
                           <Tween
                             from={{
-                              y: 50, x: 0, scale: 2, visibility: 'hidden', opacity: 0
+                              y: 50, opacity: 0
                             }}
                             to={{
-                              y: 0, x: 0, scale: 1, visibility: 'visible', opacity: 1
+                              y: 0, opacity: 1
                             }}
-                            stagger={{ from: 'start', amount: 1, grid: [4, 1] }}
-                          >
-                            <h3>Worldwide. World Class.</h3>
-                            <h4>40+ Happy Clients</h4>
-                            <h4>80+ Projects Completed</h4>
-                            <h4>8+ Years a Developer</h4>
-                          </Tween>
+                            stagger={{
+                              from: 'start', amount: 1, duration: 1
+                            }}
+                          />
+                          <Tween
+                            to={{
+                              y: -100, opacity: 0
+                            }}
+                            stagger={{
+                              from: 'start', amount: 1, duration: 1
+                            }}
+                          />
                         </Timeline>
                       </GlobeTextContainer>
                     </ContainerContentStandard>
