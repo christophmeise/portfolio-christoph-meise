0b565596ad9b1b80670e3aa91daecc1d37fb2abc
Shark919 <christoph.meise@gmx.de>
2022-04-10 17:19:13 +0200

    Fix deps

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 
-import LoadablePlugin from '@loadable/webpack-plugin';
+const LoadablePlugin = require('@loadable/webpack-plugin');
 
 /** @type { import("gatsby").GatsbyNode } */
 
