84144943a46d47fca0c91fb617985e02d55af32f
Shark919 <christoph.meise@gmx.de>
2022-04-10 17:08:09 +0200

    Gatsby fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
-const { createPages } = require('./lib/bootup/createPages');
+// const { createPages } = require('./lib/bootup/createPages');
 const { onCreateWebpackConfig } = require('./lib/bootup/webpack');
 
 /** @type { import("gatsby").GatsbyNode } */
 const config = {};
 exports.config = config;
 
-config.createPages = createPages;
+// config.createPages = createPages;
 config.onCreateWebpackConfig = onCreateWebpackConfig;
 
 module.exports = config;
