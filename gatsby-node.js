3ecfdfbfb6fe12297393b2c7d95ddfe3d8436657
Shark919 <christoph.meise@gmx.de>
2022-04-10 17:16:28 +0200

    Fix deps

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,31 @@
-// const { createPages } = require('./lib/bootup/createPages');
-const { onCreateWebpackConfig } = require('./lib/bootup/webpack');
+/* eslint-disable no-unused-vars */
+
+import LoadablePlugin from '@loadable/webpack-plugin';
 
 /** @type { import("gatsby").GatsbyNode } */
+
+export const createPages = async ({
+  graphql,
+  actions: { createPage }
+}) => {
+  // Create your pages
+};
+
+export const onCreateWebpackConfig = async ({
+  stage,
+  loaders,
+  actions
+}) => {
+  actions.setWebpackConfig({
+    plugins: [new LoadablePlugin()]
+  });
+};
+
+
 const config = {};
 exports.config = config;
 
-// config.createPages = createPages;
+config.createPages = createPages;
 config.onCreateWebpackConfig = onCreateWebpackConfig;
 
 module.exports = config;
